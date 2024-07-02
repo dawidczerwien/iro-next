bfca1747b1b8603d0dc08b04cece5c9f272b7bf9
dawidczerwien <dawidcze8@gmail.com>
2024-07-02 23:12:19 +0200

    changed images & styles

--- src/components/Footer/Newsletter.js
+++ src/components/Footer/Newsletter.js
@@ -9,24 +9,7 @@ export default function Newsletter() {
           <p>
           Instytut Rozwoju Orawy to przestrzeń, w której każdy może znaleźć swoje miejsce i przyczynić się do pozytywnych zmian w naszym regionie. Zapraszamy do współpracy przedsiębiorców, mieszkańców, którzy chcą wspólnie działać na rzecz lepszej przyszłości Orawy.
           </p>
-          <form
-            action="mail/mail.php"
-            method="get"
-            className="newsletter-inner"
-          >
-            <input
-              name="email"
-              placeholder="Email"
-              className="common-input"
-              // onfocus="this.placeholder = ''"
-              // onblur="this.placeholder = 'Your email address'"
-              required=""
-              type="email"
-            />
-            <Link className="button" href="/contact">
-              <i className="icofont icofont-paper-plane m-2 p-1"></i>
-            </Link>
-          </form>
+          
         </div>
       </div>
     </>
